8500c1de184e9bb1150c00582e15683bc47ce639
Bristow-Scripts <jordon@bristow.ca>
2026-06-05 11:15:43 -0600

    Update TECH---Force-Contact-Section-Collapsed.user.js

--- TECH---Force-Contact-Section-Collapsed.user.js
+++ TECH---Force-Contact-Section-Collapsed.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TECH - Force Contact Section Collapsed
-// @match        https://bristow-app.azurewebsites.net/*
+// @version      1.2
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/TECH---Force-Contact-Section-Collapsed.user.js
 // @downloadURL  https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/TECH---Force-Contact-Section-Collapsed.user.js
 // @match        https://bristow-app.azurewebsites.net/Orders/Orders/Edit*
