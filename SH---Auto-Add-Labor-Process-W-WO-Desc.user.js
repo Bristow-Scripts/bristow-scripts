432bc668c0e84483860d8f359c26cc40a9bd7766
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 00:21:49 -0600

    Update SH---Auto-Add-Labor-Process-W-WO-Desc.user.js

--- SH---Auto-Add-Labor-Process-W-WO-Desc.user.js
+++ SH---Auto-Add-Labor-Process-W-WO-Desc.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         SH - Process W/ WO Desc.
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version 3.2
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/SH---Auto-Add-Labor-Process-W-WO-Desc.user.js
 // @downloadURL  https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/SH---Auto-Add-Labor-Process-W-WO-Desc.user.js
 // @description  Automatically fills work order description
-// @match        https://bristow-app.azurewebsites.net/Orders/Orders/Edit*
+// @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/Orders/Orders/Edit*
 // @grant        none
 // @tag          SH
 // ==/UserScript==
