fc4fc0aaad3568fc58e419afbb954fe8d7f3e35b
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 08:00:02 -0600

    Update ALL---Uniform-Date-Format-DD-MMM-YYYY.user.js

--- ALL---Uniform-Date-Format-DD-MMM-YYYY.user.js
+++ ALL---Uniform-Date-Format-DD-MMM-YYYY.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ALL - Uniform Date Format (DD-MMM-YYYY)
 // @namespace    https://bristow-scripts.github.io/bristow-scripts
-// @version 1.4
+// @version      1.5
 // @description  Reformats all visible dates on the page to DD-MMM-YYYY
 // @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/*
 // @noframes
