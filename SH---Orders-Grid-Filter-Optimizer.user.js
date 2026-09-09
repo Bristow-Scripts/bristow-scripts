7724578530114c8e61847ddd09fca3470d11ae17
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 12:34:14 -0600

    Update SH---Orders-Grid-Filter-Optimizer.user.js

--- SH---Orders-Grid-Filter-Optimizer.user.js
+++ SH---Orders-Grid-Filter-Optimizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SH - Orders Grid Filter Optimizer
 // @namespace    http://tampermonkey.net/
-// @version      7.5
+// @version      7.6
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/SH---Orders-Grid-Filter-Optimizer.user.js
 // @downloadURL  https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/SH---Orders-Grid-Filter-Optimizer.user.js
 // @description  WIP, Print, Clear buttons. Defaults filters to contains.
