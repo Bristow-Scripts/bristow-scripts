eef92317a190a89dc4e27833f6243ea2db9b9746
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 00:16:35 -0600

    Update ALL---Library-Control-Sheet-Print.user.js

--- ALL---Library-Control-Sheet-Print.user.js
+++ ALL---Library-Control-Sheet-Print.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         ALL - Library Control Sheet Print
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version 1.4
 // @description  Retired — replaced by LIB - Document / Library Control
 // @author       You
-// @match        https://bristow-app.azurewebsites.net/Catalog/Documentations/EditDocumentation*
+// @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/Catalog/Documentations/EditDocumentation*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/ALL---Library-Control-Sheet-Print.user.js
 // @downloadURL  https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/ALL---Library-Control-Sheet-Print.user.js
