755e199753b35ce3163bfa86de69b4cb5b125341
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 00:24:00 -0600

    Update TECH---Orders-Grid-Filter-Optimizer.user.js

--- TECH---Orders-Grid-Filter-Optimizer.user.js
+++ TECH---Orders-Grid-Filter-Optimizer.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         TECH - Orders Grid Filter Optimizer
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version 1.5
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/TECH---Orders-Grid-Filter-Optimizer.user.js
 // @downloadURL  https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/TECH---Orders-Grid-Filter-Optimizer.user.js
 // @description  Tech version of Orders Grid Optimizer. Same as SH version but defaults Show Complete & Cancelled to OFF.
-// @match        https://bristow-app.azurewebsites.net/Orders/Orders
+// @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/Orders/Orders
 // @grant        none
 // ==/UserScript==
