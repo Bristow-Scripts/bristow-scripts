bd3627083b65aee97d054aab9ae9bf16feba766b
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 08:02:58 -0600

    Update SH---AeroTools-Manager.user.js

--- SH---AeroTools-Manager.user.js
+++ SH---AeroTools-Manager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SH - AeroTools Manager
 // @namespace    http://tampermonkey.net/
-// @version 3.3
+// @version      3.4
 // @description  Retired — replaced by LIB - AeroTools Manager
 // @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/Catalog/AeroTools*
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/SH---AeroTools-Manager.user.js
