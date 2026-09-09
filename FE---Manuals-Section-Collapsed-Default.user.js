225a0efec71aece292455f17ec49a5a0b2899a07
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 00:18:41 -0600

    Update FE---Manuals-Section-Collapsed-Default.user.js

--- FE---Manuals-Section-Collapsed-Default.user.js
+++ FE---Manuals-Section-Collapsed-Default.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         FE - Manuals Section Collapsed Default
 // @namespace    http://tampermonkey.net/
-// @version      1.8
+// @version 1.9
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/FE---Manuals-Section-Collapsed-Default.user.js
 // @downloadURL  https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/FE---Manuals-Section-Collapsed-Default.user.js
 // @description  Re-injects collapse after DOM refresh (Kendo/Razor safe)
-// @match        https://bristow-app.azurewebsites.net/Orders/Orders/Edit*
+// @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/Orders/Orders/Edit*
 // @grant        none
 // @tag          FE
 // ==/UserScript==
