50fe57a2a518c5959e1e02779143f8f5bf638751
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 12:52:32 -0600

    Update FE---Force-Contact-Section-Expanded.user.js.

--- FE---Force-Contact-Section-Expanded.user.js
+++ FE---Force-Contact-Section-Expanded.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         FE - Force Contact Section Expanded
-// @version      1.2
+// @version      1.3
 // @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/Orders/Orders/Edit*
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/FE---Force-Contact-Section-Expanded.user.js
 // @downloadURL  https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/FE---Force-Contact-Section-Expanded.user.js
 // @description  Keep Contact section always expanded
-// @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net//Orders/Orders/Edit*
+// @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/Orders/Orders/Edit*
 // @grant        none
 // @tag          FE
 // ==/UserScript==
