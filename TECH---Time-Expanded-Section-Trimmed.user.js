384685a94a88ed58033a7c0099d4bc4d56aa5f21
Bristow-Scripts <jordon@bristow.ca>
2026-09-09 00:25:45 -0600

    Update TECH---Time-Expanded-Section-Trimmed.user.js

--- TECH---Time-Expanded-Section-Trimmed.user.js
+++ TECH---Time-Expanded-Section-Trimmed.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         TECH - Time Expanded Section Trimmed
 // @namespace    http://tampermonkey.net/
-// @version      4.3
+// @version 4.4
 // @updateURL    https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/TECH---Time-Expanded-Section-Trimmed.user.js
 // @downloadURL  https://raw.githubusercontent.com/Bristow-Scripts/bristow-scripts/main/TECH---Time-Expanded-Section-Trimmed.user.js
-// @match        https://bristow-app.azurewebsites.net/Orders/Orders/Edit*
+// @match        https://liquid-264-drc0bgd0eje0ckcg.westus3-01.azurewebsites.net/Orders/Orders/Edit*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
